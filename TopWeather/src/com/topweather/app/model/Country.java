3940e376a0979057569375fd15b8b0c72e3f6364
ring04w <ring04w@gmail.com>
2015-04-09 16:53:07 +0800

    Add city country province labels and mode.class

--- TopWeather/src/com/topweather/app/model/Country.java
+++ TopWeather/src/com/topweather/app/model/Country.java
@@ -1,5 +1,42 @@
 package com.topweather.app.model;
 
 public class Country {
+	private int id;
+	private String countryName;
+	private String countryCode;
+	private String cityId;
+	
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getCountryName(){
+		return countryName;
+	}
+	
+	public void setCountryName(String countryName){
+		this.countryName = countryName;
+	}
+	
+	public String getCountryCode(){
+		return countryCode;
+	}
+	
+	public void setCountry(String countryCode){
+		this.countryCode = countryCode;
+	}
+	
+	public String getCityId(){
+		return cityId;
+	}
+	
+	public void setCityId(String cityId){
+		this.cityId = cityId;
+	}
 
 }
